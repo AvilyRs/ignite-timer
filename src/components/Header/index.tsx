463ffbe2d75454840ac55b6e9854cf203731efd4
Avily Silva <avily.sva@gmail.com>
2023-03-30 14:28:04 -0300

    [component] header was styled

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import { Timer, Scroll } from 'phosphor-react';
+import { NavLink } from 'react-router-dom';
+
+import logoIgnite from '../../assets/logo-ignite.svg';
+
+import { HeaderContainer } from './styles';
+
+export function Header() {
+  const menuIconSize = 24;
+
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="Dois Triângulos verdes" />
+      <nav>
+        <NavLink to="/" title='Timer'>
+          <Timer
+            size={menuIconSize}
+          />
+        </NavLink>
+        <NavLink to="/history" title='History'>
+          <Scroll size={menuIconSize} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
